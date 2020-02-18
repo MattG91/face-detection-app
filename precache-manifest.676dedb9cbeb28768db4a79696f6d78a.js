2fba3de3b7208aebf0a772ba4120ed48e3de4093
Matthew Guest <Matthew.Guest@cabelas.com>
2020-02-18 07:12:53 -0700

    Updates

---
+++ precache-manifest.676dedb9cbeb28768db4a79696f6d78a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f3afa8d7a77827edf8d9a1631d60034",
+    "url": "/face-detection-app/index.html"
+  },
+  {
+    "revision": "f24d9018c38c0b9cf6c5",
+    "url": "/face-detection-app/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a6f0270a6c3faac7d645",
+    "url": "/face-detection-app/static/css/main.a300c68a.chunk.css"
+  },
+  {
+    "revision": "f24d9018c38c0b9cf6c5",
+    "url": "/face-detection-app/static/js/2.d79a78d4.chunk.js"
+  },
+  {
+    "revision": "a6f0270a6c3faac7d645",
+    "url": "/face-detection-app/static/js/main.4d02838c.chunk.js"
+  },
+  {
+    "revision": "d5a39258008950062ffb",
+    "url": "/face-detection-app/static/js/runtime-main.7d6f7256.js"
+  },
+  {
+    "revision": "c699d50d1540fa5052673415ec8e0926",
+    "url": "/face-detection-app/static/media/brain-1710293_640.c699d50d.png"
+  }
+]);
